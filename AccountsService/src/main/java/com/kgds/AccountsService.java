82a905cfd86802ae1bf106298f185674b0f1aa50
Krishna Sharma <kgdsharma@gmail.com>
2024-08-03 14:21:22 -0400

    first commit

---
+++ AccountsService/src/main/java/com/kgds/AccountsService.java
@@ -0,0 +1,12 @@
+package com.kgds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsService {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AccountsService.class, args);
+        }
+}
---
+++ CustomersService/src/main/java/com/kgds/CustomersService.java
@@ -0,0 +1,13 @@
+package com.kgds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersService.class, args);
+    }
+
+}
