31eabeaf8898b31cfb3de67fb087b683500f994f
Krishna Sharma <kgdsharma@gmail.com>
2024-08-03 14:36:32 -0400

    Enhanced CI

--- CustomersService/src/main/java/com/kgds/CustomersService.java
+++ CustomersService/src/main/java/com/kgds/CustomersService.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomersService {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomersService.class, args);
-    }
 
+        SpringApplication.run(CustomersService.class, args);
+        System.out.println("Hello World");
+    }
 }
