be8df4da4e801f48baf3ee97f303e3f9aca26ff5
Krishna Sharma <kgdsharma@gmail.com>
2024-08-03 23:25:10 -0400

    Enhanced-CI

--- CustomersService/src/main/java/com/kgds/CustomersService.java
+++ CustomersService/src/main/java/com/kgds/CustomersService.java
@@ -11,7 +11,7 @@ public class CustomersService {
         System.out.println("Service started successfully");
         System.out.println("Service started successfully on port 8081");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Service started successfully on port 8084");
+            System.out.println("Service started successfully on port 8085");
 
         }
     }
