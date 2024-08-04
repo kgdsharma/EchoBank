76d6c7a4eb93a8b6b92b235459121ef2c4cb84ce
Krishna Sharma <kgdsharma@gmail.com>
2024-08-04 15:27:15 -0400

    Enhanced CI Final-V2

--- AccountsService/src/main/java/com/kgds/AccountsService.java
+++ AccountsService/src/main/java/com/kgds/AccountsService.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountsService {
 
         public static void main(String[] args) {
-            SpringApplication.run(AccountsService.class, args);
-            System.out.println("Hello World !");
-        }
+            SpringApplication.run(AccountsService.class, args);}
 }
--- CustomersService/src/main/java/com/kgds/CustomersService.java
+++ CustomersService/src/main/java/com/kgds/CustomersService.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomersService {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomersService.class, args);
-        System.out.println("Hello World !");}
-
+        SpringApplication.run(CustomersService.class, args);}
 }
