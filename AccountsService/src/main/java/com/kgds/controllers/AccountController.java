4271a82d2ccfa46deadcd7992a9d8ff4250564c9
Krishna Sharma <kgdsharma@gmail.com>
2024-08-04 03:27:25 -0400

    fixed project model

--- AccountsService/src/main/java/com/kgds/controllers/AccountController.java
+++ AccountsService/src/main/java/com/kgds/controllers/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
 
 @RestController
@@ -14,7 +15,8 @@ public class AccountController {
 
         List<Account> accounts = List.of(
                 Account.builder().CustomerId("1").accountNumber("1234").accountType("Savings").build(),
-                Account.builder().CustomerId("2").accountNumber("5678").accountType("Current").build()
+                Account.builder().CustomerId("2").accountNumber("5678").accountType("Current").build(),
+                Account.builder().CustomerId("3").accountNumber("91011").accountType("Savings").build()
 
         );
         return ResponseEntity.ok(accounts);
