338b5fd4102fd190fae942af1c3704d8fe96df36
Krishna Sharma <kgdsharma@gmail.com>
2024-08-03 20:41:45 -0400

    Test Enhanced CI

--- AccountsService/src/main/java/com/kgds/AccountsService.java
+++ AccountsService/src/main/java/com/kgds/AccountsService.java
@@ -9,5 +9,9 @@ public class AccountsService {
         public static void main(String[] args) {
             SpringApplication.run(AccountsService.class, args);
             System.out.print("AccountsService started successfully.");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Service started successfully on port 8080");
+
+            }
         }
 }
