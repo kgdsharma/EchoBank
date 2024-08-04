a490bf7c59ed9278f85c85f5b146d2558a4a9af0
Krishna Sharma <kgdsharma@gmail.com>
2024-08-03 21:23:43 -0400

    fixing and testing prod CI again

--- CustomersService/src/main/java/com/kgds/CustomersService.java
+++ CustomersService/src/main/java/com/kgds/CustomersService.java
@@ -11,7 +11,7 @@ public class CustomersService {
         System.out.println("Service started successfully");
         System.out.println("Service started successfully on port 8081");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Service started successfully on port 8082");
+            System.out.println("Service started successfully on port 8083");
 
         }
     }
